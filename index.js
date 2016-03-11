14064d86338dcbec711b05f39d56477faf032438
Jacket2013 <342115615@qq.com>
2016-03-11 12:30:50 +0800

    add test and readme

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var getChar = function (chars) {
+    var i = Math.ceil(Math.random() * (chars.length - 1));
+    return chars[i];
+}
+var getCodeStr = function (chars, len) {
+    var r = '';
+    for (var i = 0; i < len; i++) r += getChar(chars);
+    return r;
+}
+var getItem = function (arr) {
+    var i = Math.ceil(Math.random() * (arr.length - 1));
+    return arr[i];
+}
 var force2number = function (value, defaultValue) {
     if (typeof value == 'number') return value;
     if (typeof value == 'string') {
@@ -5,13 +18,6 @@ var force2number = function (value, defaultValue) {
         return defaultValue;
     }
     return defaultValue;
-}
-
-var ValidationImage = function () {
-    this.width = 72;
-    this.height = 36;
-    this.lineNumber = 0;
-    this.charNumber = 4;
 };
 var update = function (o1, o2) {
     o1 = o1 || {}, o2 = o2 || {};
@@ -28,78 +34,83 @@ var update = function (o1, o2) {
         }
     }
     return o1;
-}
+};
 
-ValidationImage.prototype.create = function (options) {
+var ValidationImage = function () {
+    this.width = 72;
+    this.height = 36;
+    this.lineNumber = 0;
+    this.charNumber = 4;
+};
+ValidationImage.prototype.config = function (options) {
     update(this, options);
-/*
-
-    var CANVAS_WIDTH = !options.width || options.width < this.width ? this.width : force2number(options.width, this.width),
-        CANVAS_HEIGHT = !options.height || options.height < this.height ? this.height : force2number(options.height, this.height),
-        LINE_NUMS = options.lineNumber || this.lineNumber,
-        CHAR_NUMS = options.charNumber || 4;
-
-    if (typeof LINE_NUMS == 'string' && /^\d+$/.test(LINE_NUMS)) LINE_NUMS = Number(LINE_NUMS);  
-
+    return this;
+};
+ValidationImage.prototype.create = function () {
     var Canvas = require('canvas'),
         Image = Canvas.Image,
-        canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT),
+        canvas = new Canvas(this.width, this.height),
         ctx = canvas.getContext('2d');
 
     var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var fontSizes = [ '14px', '18px', '20px', '24px'];
     var fontFamilies = [ 'Impact', 'Arial', 'Georgia', 'sans-serif', 'Helvetica', 'monospace' ];
     var colors = [ 'rgba(0, 0, 0, 0.5)', 'rgba(22, 85, 255, 0.5)', 'rgba(97, 18, 132, 0.5)', 'rgba(67, 67, 67, 0.5)', 'rgba(228, 0, 16, 0.5)' ];
-    var pos = [ { x: 10, y: CANVAS_HEIGHT / 2 }, { x: 0, y: CANVAS_HEIGHT * 8 / 9 } ];
-    var getChar = function (chars) {
-        var i = Math.ceil(Math.random() * (chars.length - 1));
-        return chars[i];
-    }
-    var getItem = function (arr) {
-        var i = Math.ceil(Math.random() * (arr.length - 1));
-        return arr[i];
-    }
-    var getCodeStr = function (chars, len) {
-        var r = '';
-        for (var i = 0; i < len; i++) r += getChar(chars);
-        return r;
-    }
-    var strokeLine = function () {
+    var pos = [ { x: 10, y: this.height / 2 }, { x: 0, y: this.height * 8 / 9 } ];
+    var lines = this.lineNumber;
+
+    var strokeLine = function (width, height, colors) {
         var lineStart = {
-            x: Math.random() * CANVAS_WIDTH / 3,
-            y: Math.random() * CANVAS_HEIGHT
+            x: Math.random() * width / 3,
+            y: Math.random() * height
         };
         var lineEnd = {
-            x: Math.random() * CANVAS_WIDTH,
-            y: Math.random() * CANVAS_HEIGHT
+            x: Math.random() * width,
+            y: Math.random() * height
         };
-        // line
         ctx.beginPath();
         ctx.strokeStyle = getItem(colors);
         ctx.moveTo(lineStart.x, lineStart.y);
         ctx.lineTo(lineEnd.x, lineEnd.y);
         ctx.stroke();
-    }
+    };
 
     var fontSize = getItem(fontSizes),
         fontFamily = getItem(fontFamilies),
-        char = getCodeStr(chars, CHAR_NUMS),
+        code = getCodeStr(chars, this.charNumber),
         color = getItem(colors),
         deg = (Math.random() * 2 - 1) * (Math.PI / 12),
         pos = deg > 0 ? pos[0] : pos[1],
         baseLine = deg > 0 ? 'top' : 'bottom';
 
-    // chars
+    this.code = code;
+
+    // draw chars
     ctx.font = fontSize + ' ' + fontFamily;
     ctx.fillStyle = color;
     ctx.textBaseLine = baseLine;
     ctx.rotate(deg);
-    ctx.fillText(char, pos.x, pos.y);
+    ctx.fillText(code, pos.x, pos.y);
 
-    while (LINE_NUMS--) strokeLine();
+    // draw lines
+    while (lines--) strokeLine(this.width, this.height, colors);
 
-    return canvas.toDataURL();
-    */
-}
+    this.base64URL = canvas.toDataURL();
+    return this;
+};
+ValidationImage.prototype.getCode = function () {
+    if (!this.code) {
+        console.warn('ValidationImage warning: cann\'t getCodes before ValidationImage#create() has been called.');
+        this.code = '';
+    }
+    return this.code;
+};
+ValidationImage.prototype.getBase64URL = function () {
+    if (!this.base64URL) {
+        console.warn('ValidationImage warning: cann\'t getBase64URL before ValidationImage#create() has been called.');
+        this.base64URL = '';
+    }
+    return this.base64URL;
+};
 
 module.exports = new ValidationImage;
--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,20 @@
-var assert = require('assert');
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect;
+chai.should();
 
 describe('index.js', function() {
   var validationImage;
-  
+
   before(function () {
     delete require.cache[require.resolve(process.cwd() + '/index.js')];
     validationImage = require('../index');
   });
-  
+
   after(function () {
     validationImage = null;
   });
-  
+
   it('validationImage should be an object', function () {
     assert.equal('object', typeof validationImage);
   });
@@ -28,7 +31,8 @@ describe('index.js', function() {
     assert.equal(4, validationImage.charNumber);
   });
 
-  describe('validationImage#create(options)', function () {
+  // validationImage#config(options)
+  describe('validationImage#config(options)', function () {
     var validationImage;
 
     beforeEach(function () {
@@ -39,27 +43,121 @@ describe('index.js', function() {
       validationImage = null;
     });
 
-    it('validationImage#create should be a function', function () {
-      assert.equal('function', typeof validationImage.create);
+    it('validationImage#config should be a function', function () {
+      assert.equal('function', typeof validationImage.config);
     });
-    it('validationImage\'s properties should be the default value when the argument `options` of validationImage#create is null', function () {
-      validationImage.create();
+    it('validationImage\'s properties should be the default value when the argument `options` of validationImage#config is null', function () {
+      validationImage.config();
       assert.equal(72, validationImage.width);
       assert.equal(36, validationImage.height);
       assert.equal(0, validationImage.lineNumber);
       assert.equal(4, validationImage.charNumber);
     });
     it('options.width = 80, validationImage.width should be 80', function () {
-      validationImage.create({ width: 80 });
+      validationImage.config({ width: 80 });
       assert.equal(80, validationImage.width);
     });
     it('options.width = \'80\', validationImage.width should be 80', function () {
-      validationImage.create({ width: '80' });
+      validationImage.config({ width: '80' });
       assert.equal(80, validationImage.width);
     });
     it('options.width = \'8ac\', validationImage.width should be the default value: 72', function () {
-      validationImage.create({ width: '8ac' });
+      validationImage.config({ width: '8ac' });
       assert.equal(72, validationImage.width);
     });
+    it('validationImage\'s properties should be successfully updated', function () {
+      validationImage.config({
+        width: 82,
+        height: 32,
+        lineNumber: '6',
+        charNumber: '5a'
+      });
+      assert.equal(82, validationImage.width);
+      assert.equal(32, validationImage.height);
+      assert.equal(6, validationImage.lineNumber);
+      assert.equal(4, validationImage.charNumber);
+    });
   });
+
+  // validationImage#create()
+  describe('validationImage#create()', function () {
+    var validationImage;
+
+    beforeEach(function () {
+      delete require.cache[require.resolve(process.cwd() + '/index.js')];
+      validationImage = require('../index');
+    });
+    afterEach(function () {
+      validationImage = null;
+    });
+
+    it('should create a validation code', function () {
+      var obj = validationImage.config({ charNumber: 6 }).create();
+      expect(obj.code).to.match(/^[a-zA-Z0-9]+$/);
+      assert.equal(6, obj.code.length);
+    });
+    it('should create a base64 url', function () {
+      var obj = validationImage.create();
+      expect(obj.base64URL).to.match(/^data:image\/\w+;base64,/);
+    });
+  });
+
+  // validationImage#createBase64URL()
+  describe('validationImage#getBase64URL()', function () {
+    var validationImage;
+
+    beforeEach(function () {
+      delete require.cache[require.resolve(process.cwd() + '/index.js')];
+      validationImage = require('../index');
+    });
+    afterEach(function () {
+      validationImage = null;
+    });
+
+    it('should return a base64 url', function () {
+      var url = validationImage.create().getBase64URL();
+      expect(url).to.match(/^data:image\/\w+;base64,/);
+    });
+  });
+
+  // validationImage#getCodes()
+  describe('validationImage#getCodes()', function () {
+    var validationImage;
+
+    beforeEach(function () {
+      delete require.cache[require.resolve(process.cwd() + '/index.js')];
+      validationImage = require('../index');
+    });
+    afterEach(function () {
+      validationImage = null;
+    });
+
+    it('should return a validation code(string)', function () {
+      var code = validationImage.create().getCode();
+      expect(code).to.match(/^[a-zA-Z0-9]+$/);
+    });
+  })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
